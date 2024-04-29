1d63b923c55460500e70b94dbb2a4859c5ec9cb3
johnworker <besscomdark@gmail.com>
2024-04-29 11:07:41 +0800

    新增內容

---
+++ main.js
@@ -0,0 +1,48 @@
+// wrapper區
+$(document).ready(function() {
+    var $slider = $('.spot_bg_show');
+    $slider.slick({
+        initialSlide: 0,
+        infinite: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        vertical: false,
+        arrows: false,
+        draggable: false,
+        rtl: false,
+        autoplay: false,
+        speed: 600
+    });
+
+    var scrollTrigger = 450;
+    var totalSlides = $slider.slick('getSlick').slideCount;
+
+    $(window).scroll(function() {
+        var st = $(this).scrollTop();
+        var relativeScroll = st - scrollTrigger;
+        
+        if (st >= scrollTrigger) {
+            var slideIndex = Math.max(0, Math.min(Math.floor(relativeScroll / (1000 / totalSlides)), totalSlides - 1));
+            $slider.slick('slickGoTo', slideIndex, false);
+        } else {
+            $slider.slick('slickGoTo', 0, true); // 保持在第一張圖片
+        }
+    });
+});
+
+
+// recommend區
+$(document).ready(function() {
+    var $slider = $('.recommend_slider');
+    $slider.slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        draggable: false,
+        autoplay: true,
+        pauseOnHover: true,
+        autoplaySpeed: 0,
+        speed: 1000
+    });
+});
