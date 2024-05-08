a148e6a2c23030f655ce2aea7ff7f97eb5579896
johnworker <besscomdark@gmail.com>
2024-05-08 16:29:03 +0800

    調整

--- main.js
+++ main.js
@@ -32,6 +32,23 @@ $(document).ready(function() {
 });
 
 
+// wrapper test
+document.addEventListener('scroll', function() {
+    var scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+    var maxScroll = document.body.scrollHeight - window.innerHeight;
+
+    // 計算移動的比例
+    var movePercentage = scrollPosition / maxScroll;
+
+    // 根據滾動的百分比來計算移動距離（這裡的 1000 是示例值，可以根據實際內容調整）
+    var moveX = movePercentage * 10000; // 可以調整這個數值來控制移動的範圍
+
+    // 應用 GSAP 動畫
+    gsap.to('.spot_bg_show', {x: -moveX}); // 向左移動，如果需要向右移動可以改變符號
+});
+
+
+
 // recommend區
 $(document).ready(function() {
     var $slider = $('.recommend_slider');
