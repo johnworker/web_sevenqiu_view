b0c03b4567290cad40717caaa96c41ad0f7e7c51
johnworker <besscomdark@gmail.com>
2024-05-29 14:39:37 +0800

    更新漢堡選單

--- main.js
+++ main.js
@@ -124,3 +124,10 @@ $('.service_img').slick({
     pauseOnHover: true,
     arrows: false
   });
+
+
+// 選單效果
+$(".menu_btn").click(function () {
+    $(".menu_wrap").toggleClass("active");
+    $(this).toggleClass("active");
+  });
